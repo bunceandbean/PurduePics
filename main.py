4810a634f2805f0e687ebf79ade48ec2a5f3b6cc
Ben Lilley <benlilleydev@gmail.com>
2022-10-03 21:11:37 -0400

    Moving to PythonAnywhere

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import tweepy
-from time import sleep
-from datetime import datetime as dt
-import os
+from credentials import *
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.environ["API_key"], os.environ["API_secret_key"])
-auth.set_access_token(os.environ["access_token"], os.environ["access_token_secret"])
+auth = tweepy.OAuthHandler(API_key, API_secret_key)
+auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-while True:
-    if dt.now().hour == 20:
-        try:
-            api.verify_credentials()
-            print("Authentication OK")
-            api.update_status_with_media(status="Test",filename="test.jpg")
-        except:
-            print("Error during authentication")
-        sleep(60)
-    else:
-        sleep(5)
+# Make Tweet
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+    api.update_status_with_media(status="Test",filename="test.jpg")
+except:
+    print("Error during authentication")
